40277aae593bff03f56bf108afb0617d091757a1
Klaus Paschek <klpaschek@gmail.com>
2020-02-07 13:17:10 +0100

    First simple working port from FORTRAN to C++

--- nucleobaseSynthesis.cpp
+++ nucleobaseSynthesis.cpp
@@ -8,6 +8,17 @@
  * Sources:     ChemApp Programmer's Manual
  * (https://gtt-technologies.de/software/chemapp/documentation/online-manual/,
  * January 2020)
+ *
+ * Note: Be aware that often loop indices follow the FORTRAN index convention
+ * by starting with 1, not 0, to make the ChemApp routines work properly. When
+ * using them for element access within C/C++ containers, they often have to be
+ * reduced by 1 so that again these containters behave correctly.
+ * Example:
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * for (LI i(1); i <= size; ++i)    // LI is int usable with ChemApp routines
+ *     tq...(..., i, ..., &err);    // ChemApp routine
+ *     someVariable = someContainer[i - 1];
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
 
 #include <iostream>
@@ -15,7 +26,13 @@
 #include <cstddef>
 #include <cstdlib>
 #include <iomanip>
+#include <fstream>
+#include <vector>
+#include <iterator>
+#include <utility>
+#include <cstring>
 
+// ChemApp
 #include "cacint.h"
 
 
@@ -32,6 +49,29 @@ void abortProg(int lineNo, std::string funcName, LI errorNo)
 
 int main()
 {
+    ////
+    // Wanted nucleobase
+    ////
+    char nucleobaseStr[TQSTRLEN] = "C5H5N5";
+
+
+    ////
+    // Input/output file names
+    ////
+
+    // Input
+    std::string thermoChemFileName("moleculeData100barAdenine-3.dat");
+    std::string tempsFileName     ("inputPressureTemps.dat");
+    std::string initConcFileName  ("initialConcentrations.dat");
+
+    // Output
+    // File containing nucleobase amounts as main result of program
+    std::string amountsFileName   ("adenine100barWaterRatio-3.dat");
+    // File containing molar masses and conditions for individual equilibrium
+    // calculations
+    std::string molMassFileName   ("moleMasses.dat");
+
+
     // Error return variable that will be checked after each call of a ChemApp
     // routine
     LI err;
@@ -133,8 +173,7 @@ int main()
               << "associated with unit " << unitNo << "\n\n";
 
     // Open ASCII thermochemical data-file for reading
-    char fileName[32] = "moleculeData100barAdenine-3.dat";
-    tqopna(fileName, unitNo, &err);
+    tqopna((char*)thermoChemFileName.c_str(), unitNo, &err);
     if (err) abortProg(__LINE__, "tqopna", err);
 
     // Read data-file
@@ -181,15 +220,17 @@ int main()
     LI nSCom;
     tqnosc(&nSCom, &err);
     if (err) abortProg(__LINE__, "tqnosc", err);
+
     std::cout << "SYSTEM COMPONENTS\nNumber of system compoents: " << nSCom
               << std::endl;
 
     // Print the names of the system components and their masses
     std::cout << "No.  Name of component  Mass [g/mol]" << std::endl;
-    for (int i(1); i <= nSCom; ++i)
+    for (LI i(1); i <= nSCom; ++i)
     {
         tqgnsc(i, dstr, &err);
         if (err) abortProg(__LINE__, "tqgnsc", err);
+
         DB wMass;
         DB stoi[nSCom];
         tqstsc(i, stoi, &wMass, &err);
@@ -205,15 +246,18 @@ int main()
     char pName[TQSTRLEN];
     tqnop(&nPhase, &err);
     if (err) abortProg(__LINE__, "tqnop", err);
+
     // Print the names of the phases and their model names
     std::cout << "PHASES\nNumber of phases: " << nPhase
               << "\nNo.  Name of phase  Model" << std::endl;
-    for (int i(1); i <= nPhase; ++i)
+
+    for (LI i(1); i <= nPhase; ++i)
     {
         tqgnp(i, pName, &err);
         if (err) abortProg(__LINE__, "tqgnp", err);
         tqmodl(i, dstr, &err);
         if (err) abortProg(__LINE__, "tqmodl", err);
+
         std::cout << i << "    " << std::setw(15) << std::left << pName
                   << dstr << std::endl;
     }
@@ -224,30 +268,36 @@ int main()
     LI nPCon;
     tqnopc(1, &nPCon, &err);
     if (err) abortProg(__LINE__, "tqnopc", err);
+
     // Print the names of the phase constituents and their Gibbs free energies
     // of formation
     std::cout << "PHASE CONSTITUENTS\nNumber of phase constituents of the "
               << "first gas phase: " << nPCon
               << "\nPh.  Name of constituent  Gibbs [J/mol]\n";
+
     char pCName[TQSTRLEN];
-    for (int i(1); i <= nPhase; ++i)
+    for (LI i(1); i <= nPhase; ++i)
     {
         tqgnp(i, pName, &err);
         if (err) abortProg(__LINE__, "tqgnp", err);
         tqnopc(i, &nPCon, &err);
         if (err) abortProg(__LINE__, "tqnopc", err);
-        for (int j(1); j <= nPCon; ++j)
+
+        for (LI j(1); j <= nPCon; ++j)
         {
             tqgnpc(i, j, pCName, &err);
             if (err) abortProg(__LINE__, "tqgnpc", err);
+
             LI nValv;
             DB valv[25];
             tqgdat(i, j, (char*)"H ", 1, &nValv, valv, &err);
             if (err) abortProg(__LINE__, "tqgdat", err);
+
             DB sEnthal(valv[0]);
             tqgdat(i, j, (char*)"S ", 1, &nValv, valv, &err);
             if (err) abortProg(__LINE__, "tqgdat", err);
             DB sEntrop(valv[0]);
+
             // G = H - T*S
             DB sGibbs(sEnthal - (298.15 * sEntrop));
             std::cout << i << "    " << std::setw(19) << std::left << pCName
@@ -266,24 +316,27 @@ int main()
               << std::endl;
 
     LI nPerm(0);
-    for (int i(1); i <= nPhase; ++i)
+    for (LI i(1); i <= nPhase; ++i)
     {
         tqgnp(i, pName, &err);
         if (err) abortProg(__LINE__, "tqgnp", err);
         tqnopc(i, &nPCon, &err);
         if (err) abortProg(__LINE__, "tqnopc", err);
+
         // We only need to check mixture phases
         if (nPCon > 1)
         {
-            for (int j(1); j <= nPCon; ++j)
+            for (LI j(1); j <= nPCon; ++j)
             {
                 tqgnpc(i, j, pCName, &err);
                 if (err) abortProg(__LINE__, "tqgnpc", err);
+
                 // Check whether the current constituent is permitted to be
                 // used as incoming species
                 LI inPCIS;
                 tqpcis(i, j, &inPCIS, &err);
                 if (err) abortProg(__LINE__, "tqpcis", err);
+
                 // Print a table entry, and if it is not permitted, increase
                 // the total number of such phase constituents by 1
                 std::cout << std::setw(15) << std::left << pName
@@ -298,12 +351,222 @@ int main()
             }
         }
     }
+    std::cout << std::endl;
 
     // If there were phase constituents which cannot be used as incoming
     // species, print a note
     if (nPerm > 0)
         std::cout << "Note: " << nPerm << " phase constituent(s) is/are not "
                   << "permitted as incoming species" << std::endl;
+
+
+    ////
+    // Main Program
+    ////
+
+    // Read in initial temperatures to iterate program over.
+    // Store them in array 'temps'
+    std::ifstream tempsFile(tempsFileName);
+    std::string dummyStr;
+    DB pressure;
+    tempsFile >> dummyStr >> pressure;
+    std::istream_iterator<DB> start(tempsFile), end;
+    std::vector<DB> temps(start, end);
+
+    // Open resulting files containing nucleobase amounts and molecular masses
+    std::ofstream amountsFile(amountsFileName);
+    std::ofstream molMassFile(molMassFileName);
+
+    // Header for nucleobase amounts file
+    amountsFile << "Temperature [C],Amount [kg nucleobase/kg water]"
+                << std::endl;
+
+    // Begin iterative loop over all temperatures
+    for (DB temperature:temps)
+    {
+        // Set temperature and pressure
+        LI numCon;
+        tqsetc((char*)"T ", 0, 0, temperature, &numCon, &err);
+        if (err) abortProg(__LINE__, "tqsetc", err);
+        tqsetc((char*)"P ", 0, 0, pressure, &numCon, &err);
+        if (err) abortProg(__LINE__, "tqsetc", err);
+
+        // Obtain the inital constituent concentrations
+        std::ifstream initConcFile(initConcFileName);
+        std::istream_iterator<DB> start(initConcFile), end;
+        std::vector<DB> concs(start, end);
+
+        // Set these initial concentrations in ChemApp
+        int indexConcs(0);
+        for (LI i(1); i <= nPhase; ++i)
+        {
+            // Find number of constituents in phase
+            tqnopc(i, &nPCon, &err);
+            if (err) abortProg(__LINE__, "tqnopc", err);
+
+            for (LI j(1); j <= nPCon; ++j)
+            {
+                tqsetc((char*)"ia ", i, j, concs[indexConcs], &numCon, &err);
+                if (err) abortProg(__LINE__, "tqsetc", err);
+
+                ++indexConcs;
+            }
+        }
+
+        // Redirect the results (output of ChemApp routines like 'tqcel' and
+        // 'tqshow') to an output file for viewing
+        tqcio((char*)"LIST ", 3, &err);
+        if (err) abortProg(__LINE__, "tqcio", err);
+        tqopen((char*)"results.dat", 3, &err);
+        if (err) abortProg(__LINE__, "tqopen", err);
+
+        // Use 'tqwstr' to write header to the output file
+        tqwstr((char*)"LIST ", (char*)"RESULTS ", &err);
+        if (err) abortProg(__LINE__, "tqwstr", err);
+
+        // Calculate and write equilibrium to the output file
+        // The "  " option means there will be no target variable estimate used
+        // to begin the simulator at, meaning 'estim" istn't used
+        DB estim[1] = { 0 };
+        tqcel((char*)" ", 0, 0, estim, &err);
+        if (err) abortProg(__LINE__, "tqcel", err);
+
+        // Change 'Amount' unit to gram for calculations of ppm
+        tqcsu((char*)"Amount ", (char*)"gram ", &err);
+        if (err) abortProg(__LINE__, "tqcsu", err);
+
+        // Determine molecular masses of the phase constituents
+        std::vector< std::vector<DB> > molMass;
+        molMass.resize(nPhase);
+        for (LI i(1); i <= nPhase; ++i)
+        {
+            // Find number of constituents in phase
+            tqnopc(i, &nPCon, &err);
+            if (err) abortProg(__LINE__, "tqnopc", err);
+
+            DB wMass;
+            DB stoi[nSCom];
+            for (LI j(1); j <= nPCon; ++j)
+            {
+                tqstpc(i, j, stoi, &wMass, &err);
+                if (err) abortProg(__LINE__, "tqstpc", err);
+
+                molMass[i - 1].push_back(wMass);
+            }
+        }
+
+        // Change 'Amount' unit back to mol
+        tqcsu((char*)"Amount ", (char*)"mol ", &err);
+        if (err) abortProg(__LINE__, "tqcsu", err);
+
+        // Write temperature and pressure to molecular masses output file
+        DB value;
+        // Temperature
+        tqgetr((char*)"T ", 0, 0, &value, &err);
+        if (err) abortProg(__LINE__, "tqgetr", err);
+
+        molMassFile << "Temperature: " << value << std::endl;
+
+        // Save current temperature to write it later to other output file
+        DB currentTemp(value);
+
+        // Pressure
+        tqgetr((char*)"P ", 0, 0, &value, &err);
+        if (err) abortProg(__LINE__, "tqgetr", err);
+
+        molMassFile << "Pressure:    " << value << std::endl;
+
+        // Place equilibrium concentrations of nucleobases into an output file
+        molMassFile << "Phase constituents molar masses [g/mol]:" << std::endl;
+
+        // If water takes part in the reaction, collect corresponding indices
+        std::vector< std::pair<LI, LI> > indicesWater;
+        // Also collect indices corresponding to wanted nucleobase
+        std::vector< std::pair<LI, LI> > indicesNucleobase;
+        // Also collect molar mass of wanted nucleobase
+        DB molMassNucleobase;
+        // Furthermore collect all molar masses and write them to output file
+        for (LI i(1); i <= nPhase; ++i)
+        {
+            tqnopc(i, &nPCon, &err);
+            if (err) abortProg(__LINE__, "tqnopc", err);
+
+            for (LI j(1); j <= nPCon; ++j)
+            {
+                tqgnpc(i, j, pCName, &err);
+                if (err) abortProg(__LINE__, "tqgnpc", err);
+
+                molMassFile << std::setw(10) << std::left << pCName
+                            << molMass[i - 1][j - 1] << std::endl;
+
+                // Check if constituent is water and if collect corresponding
+                // indices
+                char h2oStr[TQSTRLEN] = "H2O";
+                if (!std::strcmp(pCName, h2oStr))
+                    indicesWater.push_back(std::make_pair(i, j));
+
+                // Check if constituent is the wanted nucleobase and collect
+                // corresponding indices & molar mass
+                if (!std::strcmp(pCName, nucleobaseStr))
+                {
+                    indicesNucleobase.push_back(std::make_pair(i, j));
+                    molMassNucleobase = molMass[i - 1][j - 1];
+                }
+            }
+        }
+        molMassFile << "\nEquilibrium amounts (species, mol, ppb)\n"
+                    << std::endl;
+
+        // If part of reaction, get concentration of water
+        DB waterConc(100.0);
+        if (indicesWater.size() != 0)
+        {
+            for (std::pair<LI, LI> indices:indicesWater)
+            {
+                DB phaseWaterConc;
+                tqgetr((char*)"A ", indices.first, indices.second,
+                       &phaseWaterConc, &err);
+                if (err) abortProg(__LINE__, "tqgetr", err);
+
+                waterConc += phaseWaterConc;
+            }
+        }
+
+        // Collect total adenine abundance in moles
+        DB nucleobaseConc(0);
+        for (std::pair<LI, LI> indices:indicesNucleobase)
+        {
+            DB phaseNucleobaseConc;
+            tqgetr((char*)"A ", indices.first, indices.second,
+                   &phaseNucleobaseConc, &err);
+            if (err) abortProg(__LINE__, "tqgetr", err);
+
+            nucleobaseConc += phaseNucleobaseConc;
+        }
+
+        // Get ratio of nucleobase X to water (kg X/kg water)
+        DB nucleobaseRatio( (nucleobaseConc * molMassNucleobase)
+                           /(waterConc      * 18.015));
+
+        // Write temperature (in °C) and nucleobase amount to file (main result
+        // of program)
+        amountsFile << currentTemp - 273.15 << "," << nucleobaseRatio
+                    << std::endl;
+    }
+
+
+    ////
+    // Closing routines
+    ////
+
+    // Close file containing redirected results of ChemApp routines like
+    // 'tqcel' and 'tqshow'
+    tqclos(3, &err);
+    if (err) abortProg(__LINE__, "tqclos", err);
+
+    // Redirect the output back to unit number 6 (standard output unit)
+    tqcio((char*)"LIST ", 6, &err);
+    if (err) abortProg(__LINE__, "tqcio", err);
 
 
     return 0;
